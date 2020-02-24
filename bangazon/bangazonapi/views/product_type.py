3a0e3c38c7e0d11a9101a8c7cf23fc55ad14a687
Charles Jackson <cpjackson572@gmail.com>
2020-02-24 14:25:21 -0600

    added serializer code

--- bangazon/bangazonapi/views/product_type.py
+++ bangazon/bangazonapi/views/product_type.py
@@ -5,3 +5,19 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from bangazonapi.models import ProductType
+
+class ProductTypeSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for intineraries
+    Arguments:
+        serializers
+    """
+
+    class Meta:
+        model = ProductType
+        url = serializers.HyperlinkedIdentityField(
+            view_name='product_type',
+            lookup_field='id'
+        )
+        #the fields are the columns you want to include in the database
+        fields = ('id', 'name')
+        depth = 2
