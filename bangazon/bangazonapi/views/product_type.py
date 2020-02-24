1e641442c88178d5bc288688bee9e998462fbcbe
Charles Jackson <cpjackson572@gmail.com>
2020-02-24 15:09:43 -0600

    added delete code in views for product types

--- bangazon/bangazonapi/views/product_type.py
+++ bangazon/bangazonapi/views/product_type.py
@@ -27,12 +27,12 @@ class ProductTypes(ViewSet):
 
     #handles POST
     def create(self, request):
-        new_producttype = ProductType()
-        new_producttype.name = request.data["name"]
+        new_product_type = ProductType()
+        new_product_type.name = request.data["name"]
 
         new_producttype.save()
 
-        serializer = ProductTypeSerializer(new_producttype, context={'request': request})
+        serializer = ProductTypeSerializer(new_product_type, context={'request': request})
 
         return Response(serializer.data)
 
@@ -50,9 +50,24 @@ class ProductTypes(ViewSet):
 
     #handles Get request to list ALL product_types
     def list(self, request):
-        product_types = ProductType.objects.all()
+        product_type = ProductType.objects.all()
 
         serializer = ProductTypeSerializer(
-            product_types, many = True, context={'request':request})
+            product_type, many = True, context={'request':request})
 
         return Response(serializer.data)
+
+
+    #handles DELETE and returns serialized detail of deleted product type
+    def destroy(self, request, pk=None):
+        try:
+            product_type = ProductType.objects.get(pk=pk)
+            product_type.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except ProductType.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
