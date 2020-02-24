437f1dc207413f0a9bc9fbd274fd7c30a4b1a3db
Charles Jackson <cpjackson572@gmail.com>
2020-02-24 14:05:35 -0600

    began views for product types and added import for product types in the views init file

---
+++ bangazon/bangazonapi/views/product_type.py
@@ -0,0 +1,7 @@
+"""View module for handling requests about product types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from bangazonapi.models import ProductType
