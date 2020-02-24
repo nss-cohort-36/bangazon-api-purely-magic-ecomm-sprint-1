4ff262a5d786e6c956e69b13399718b44aa302c9
Charles Jackson <cpjackson572@gmail.com>
2020-02-24 14:33:06 -0600

    added retrieve definition for product types)

--- bangazon/bangazonapi/views/product_type.py
+++ bangazon/bangazonapi/views/product_type.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from bangazonapi.models import ProductType
 
 class ProductTypeSerializer(serializers.HyperlinkedModelSerializer):
-    """JSON serializer for intineraries
+    """JSON serializer for product_types
     Arguments:
         serializers
     """
@@ -21,3 +21,19 @@ class ProductTypeSerializer(serializers.HyperlinkedModelSerializer):
         #the fields are the columns you want to include in the database
         fields = ('id', 'name')
         depth = 2
+
+
+class ProductTypes(ViewSet):
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single product type
+        Returns:
+            Response -- JSON serialized product type instance
+        """
+
+        try:
+            product_type = ProductType.objects.get(pk=pk)
+            serializer = ProductTypeSerializer(product_type,
+            context = {'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
