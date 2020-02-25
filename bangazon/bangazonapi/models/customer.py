9af92a55b65f9d2c4eb41b651a0721e6e20c00d8
Corri Golden <corri.golden@gmail.com>
2020-02-25 14:57:06 -0600

    update

--- bangazon/bangazonapi/models/customer.py
+++ bangazon/bangazonapi/models/customer.py
@@ -7,7 +7,7 @@ from django.dispatch import receiver
 class Customer(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # isActive = models.BooleanField(null=False)
+    
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
