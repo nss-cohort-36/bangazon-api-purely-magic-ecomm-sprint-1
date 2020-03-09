bf3613d2fc86fb22cbac3e7a3d3050928a1c0a4e
Corri Golden <corri.golden@gmail.com>
2020-03-09 14:25:56 -0500

    update

--- bangazon/bangazonapi/tests/testPaymentTypes.py
+++ bangazon/bangazonapi/tests/testPaymentTypes.py
@@ -1,30 +1,46 @@
 import json
+import unittest
 from rest_framework import status
 from django.test import TestCase
 from django.urls import reverse
-from kennywoodapi.models import ParkArea
+from bangazonapi.models import PaymentType, Customer
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
 class TestPaymentType(TestCase):
-
+   
     def setUp(self):
-        self.merchantName = 'A Negative',
-        self.accountNumber = '99988833'
-        self.expirationDate = '2024-01-01T00:00:00Z'
-        self.createdAt = '2019-01-01T00:00:00Z',
-        self.customer_id: 1
+        self.username = 'testuser'
+        self.password = 'taco'
+        self.user = User.objects.create_user(
+            username=self.username, password=self.password)
+        self.customer = Customer.objects.create(user=self.user)
+        self.token = Token.objects.create(user=self.user)
 
-    # def test_isupper(self):
-    #     self.assertTrue('FOO'.isupper())
-    #     self.assertFalse('Foo'.isupper())
+    def test_post_payment_type(self):
+        # define a payment type to be sent to be sent to the API
+        new_payment_type = {
+            "merchantName": "ZZ Negative",
+            "accountNumber": "777",
+            "expirationDate": "2029-01-01T00:00:00Z",
+            "createdAt": "2029-01-01T00:00:00Z",
+            "customer_id": 1
+        }
 
-    # def test_split(self):
-    #     string = 'hello world'
-    #     self.assertEqual(string.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #         string.split(2)
+        #  Use the client to send the request and store the response
+        response = self.client.post(
+            reverse('paymenttype-list'), new_payment_type, HTTP_AUTHORIZATION='Token ' + str(self.token)
+        )
 
-# if __name__ == '__main__':
-#     unittest.main()
+        # Getting 200 back because we have a success url
+        self.assertEqual(response.status_code, 200)
+
+        # Query the table to see if there's one PT instance in there. Since we are testing a POST request, we don't need to test whether an HTTP GET works. So, we just use the ORM to see if the thing we saved is in the db.
+        self.assertEqual(PaymentType.objects.count(), 1)
+
+        # And see if it's the one we just added by checking one of the properties. Here, name.
+        self.assertEqual(PaymentType.objects.get().merchantName, 'ZZ Negative')
+        
+    
+if __name__ == '__main__':
+    unittest.main()
